5bfa95954e4c3e85051002384507985b57e59c00
martin bedouret <martinbedouret@gmail.com>
2019-05-31 16:19:18 -0300

    Added Google TTS

---
+++ api/controllers/tts.js
@@ -0,0 +1,21 @@
+﻿const googleTTS = require('google-tts-api');
+
+
+module.exports = {
+    tts: ttsRequest
+};
+
+async function ttsRequest(req, res) {
+
+    const { body: { query, language, speed = 0.8 } } = req;
+
+    try {
+        const url = await googleTTS(query, language, speed);
+        return res.status(200).json(url);
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Error getting TTS from Google.',
+            error: err
+        });
+    }
+}
